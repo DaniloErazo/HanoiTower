2831f43316eb9610b298a25f5a50b0035011ddf2
DaniloErazo <erazodanilo1218@gmail.com>
2021-04-15 15:46:28 -0500

    Multiple entries are accpeted and the output is correct

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,40 +1,45 @@
 package ui;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 	
-	public Scanner sc = new Scanner(System.in);
-	
+	public static Scanner sc = new Scanner(System.in);
 	private static int tower;
 	private static int tower2;
 	private static int tower3;
 	
-	public static void main(String[] args) {
-		
-		Main ppal = new Main();
-		Main.setTower(3);
-		System.out.println(tower + " " + tower2 + " " + tower3);
-		Main.solveTowers(3, 1, 3);
-		
-
-	}
-	
-	public Main() {
-		setTower(0);
+	public static void main(String[] args) throws IOException {
+		tower=0;
 		tower2=0;
 		tower3=0;
-	}
-	
-	public void repeatCase(int option) {
 		
+		int repetitions = sc.nextInt();
+		sc.nextLine();
+		int[] cases = new int[repetitions];
+		int n;
+		
+		for(int i=0; i<repetitions; i++) {
+			n=sc.nextInt();
+			sc.nextLine();
+			cases[i]=n;
+		}
+		
+		for (int i = 0; i < cases.length; i++) {
+			n = cases[i];
+			tower = n;
+			tower2 = 0;
+			tower3 = 0;
+			System.out.println(n + " " + 0 + " " + 0 );
+			solveTowers(n, 1, 3);
+			System.out.println("\n");
+		}
+		
+		
+
 	}
 	
-	 
-
-	public static void setTower(int t) {
-		tower = t;
-	}
 	
 	public static void solveTowers(int n, int start, int end) {
 		if(n==1) {
@@ -52,7 +57,7 @@ public class Main {
 		}
 	}
 	
-	public static void printMove(int start, int end) {
+	public static String printMove(int start, int end) {
 		//System.out.println(start + " --> " + end);
 		if(start==1) {
 			tower=tower-1;
@@ -70,6 +75,7 @@ public class Main {
 			tower3=tower3+1;
 		}
 		System.out.println(tower + " " + tower2 + " " + tower3 );
+		return tower + " " + tower2 + " " + tower3;
 	}
 	
 
